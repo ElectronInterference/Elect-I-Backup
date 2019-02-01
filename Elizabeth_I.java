4d0febc2f6ab5d241fb2031bbab7c0ac10dc680e
ElectronInterference <43455676+ElectronInterference@users.noreply.github.com>
2019-02-01 17:29:09 -0500

    Update Elizabeth_I.java

--- Elizabeth_I.java
+++ Elizabeth_I.java
@@ -1,6 +1,7 @@
-//Copyright (c) 2017 FIRST. All rights reserved.
-
+//Copyright (c) 2017 FIRST. All rights reserved.\\
+// Libaries \\
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Hardware;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -17,113 +18,86 @@ import org.firstinspires.ftc.teamcode.MoldugaHardware;
 
 @TeleOp(name="TeleOp", group="Teleop")
 
-public class Elizabeth_I extends OpMode
+public class Elizabeth_I extends LinearOpMode 
 {
 
     //access our hardware program
     MoldugaHardware robot   = new MoldugaHardware();
     
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
-    public void init() {
+    public void runOpMode() {
         
+        //initialize the electronics
         robot.init(hardwareMap);
-
+    
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-
-        // Send power to wheels based on controller 1's joysticks
-        robot.leftfrontDrive.setPower(gamepad1.left_stick_y);
-        robot.leftrearDrive.setPower(gamepad1.left_stick_y);
-        robot.rightfrontDrive.setPower(gamepad1.right_stick_y);
-        robot.rightrearDrive.setPower(gamepad1.right_stick_y);
-
+        // updates the status
+        telemetry.update();
+        //
+        waitForStart();
         
-        //strafe if the x or b buttons are pressed
-        if(gamepad1.x == true) {
-            robot.leftfrontDrive.setPower(-0.5);
-            robot.leftrearDrive.setPower(0.5);
-            robot.rightfrontDrive.setPower(0.5);
-            robot.rightrearDrive.setPower(-0.5);
+        while(opModeIsActive() && !isStopRequested()) {
+    
+            //Sends power to wheels based on controller 1's joysticks
+            robot.leftfrontDrive.setPower(gamepad1.left_stick_y);
+            robot.leftrearDrive.setPower(gamepad1.left_stick_y);
+            robot.rightfrontDrive.setPower(gamepad1.right_stick_y);
+            robot.rightrearDrive.setPower(gamepad1.right_stick_y);
+    
             
-        } else if(gamepad1.b == true) {
-            robot.leftfrontDrive.setPower(0.5);
-            robot.leftrearDrive.setPower(-0.5);
-            robot.rightfrontDrive.setPower(-0.5);
-            robot.rightrearDrive.setPower(0.5);
-        }
-        
-        
-        //opens the Releaser mechanism
-        if (gamepad1.left_trigger == 1){
-            robot.mineralLift.setPower (1);
-
-        }
-        //Closes the releaser mechanism
-        else if (gamepad1.right_trigger == 0){
-            robot.mineralLift.setPower  (0);
+            //strafe using the x button.
+            if(gamepad1.x == true) {
+                robot.leftfrontDrive.setPower(-0.5);
+                robot.leftrearDrive.setPower(0.5);
+                robot.rightfrontDrive.setPower(0.5);
+                robot.rightrearDrive.setPower(-0.5);
+            //Strafe using the b button.    
+            } else if(gamepad1.b == true) {
+                robot.leftfrontDrive.setPower(0.5);
+                robot.leftrearDrive.setPower(-0.5);
+                robot.rightfrontDrive.setPower(-0.5);
+                robot.rightrearDrive.setPower(0.5);
+            }
             
-        }
-
-
-        
-        
-        
-        // moves the lift upwards
-        if (gamepad2.right_bumper== true ) {
-            raiseLift();
-        }
-        // If The left bumper is pressed the lift would move downwards
-        else if (gamepad2.left_bumper ==true ) {
-            lowerLift();
             
-        } 
-            
-         else {
-            stopLift();
+            //opens the Releaser mechanism
+            if (gamepad2.left_trigger == 1){
+                robot.mineralLift.setPower(1);
+    
+            }
+            //Closes the releaser mechanism
+            else if (gamepad2.right_trigger == 1){
+                robot.mineralLift.setPower(-1);
+            } else {
+                robot.mineralLift.setPower(0);
+            }
+    
+            //Moves the lift upwards
+            if (gamepad2.right_bumper == true && !robot.touch.isPressed()) {
+                raiseLift();
+            }
+            //If The left bumper is pressed move the lift downwards
+            else if (gamepad2.left_bumper == true  ) {
+                lowerLift();
+                
+            } 
+            //Stops the lift
+             else {
+                stopLift();
+            }
         }
-            
     }
 
 
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-    }
     
     // Raise the lift
     public void raiseLift() { 
-        robot.lift.setPower (1);
+        robot.lift.setPower (.80);
     }
     //Lower the lift
     public void lowerLift() {
-        robot.lift.setPower (-1);
+        robot.lift.setPower (-.80);
     }
     // Turn off the lift 
     public void stopLift() {
